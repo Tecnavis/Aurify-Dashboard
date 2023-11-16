8aa058f2ddb5735cc5ea2fb2e301c3884edf2ff6
Mohammed Sabith E <108073559+Mohammed-Sabith-E@users.noreply.github.com>
2023-11-16 14:38:44 +0530

    Edit

--- assets/js/core/login.js
+++ assets/js/core/login.js
@@ -1,6 +1,22 @@
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
-import { app } from '../../../db/db.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDz6lvz25SnnU4xNsRcwqWjg-2USXWc8E4",
+    authDomain: "aurify-80af2.firebaseapp.com",
+    projectId: "aurify-80af2",
+    storageBucket: "aurify-80af2.appspot.com",
+    messagingSenderId: "617970760313",
+    appId: "1:617970760313:web:5374c034dca547879032a8",
+    measurementId: "G-ZW6Y0TYSZT"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 const submitButton = document.getElementById("submit")
